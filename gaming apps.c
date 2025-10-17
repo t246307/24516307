10dc60e374557d9b2718ca380f5e475c6aaeda48
t246307 <t246307@st.yamagata-u.ac.jp>
2025-10-17 19:22:13 +0900

    Add files via upload

---
+++ gaming apps.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int k;
+    printf("How many gaming apps do you have?");
+    scanf("%d", &k);
+    
+    printf("I have %d game apps",k);
+
+    return 0;
+}
